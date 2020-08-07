9a9347c0d41016528d973c8b7a5d30603a2de8b7
Nicholas Elkins <nelkins2@cnm.edu>
2020-08-07 02:09:39 -0600

    Mode3 done

--- smart_room/smart_room.ino
+++ smart_room/smart_room.ino
@@ -14,13 +14,21 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-const int upper = 2;
-const int lower = 3;
-int lightnumber = 0;// bulb number 1-5 and sets it to 0
-int Brightness;//brightness number 0-255 set by encoder
-bool lights = false; //boolean to turn the lights on or off
-bool changebright;
-int color; //selects which color to set the bulbs
+const int upper = 2;  //button1 
+const int lower = 3;  //button2
+const int A = 23;     //Wire for encoder
+const int B = 22;     //Wire 2 for encoder
+int lightnumber = 0;  // bulb number 1-5 and sets it to 0
+int Brightness;       //brightness number 0-255 set by encoder
+bool lights = false;  //boolean to turn the lights on or off
+bool changebright = false;    //boolean to go into brightness change
+int color;            //selects which color to set the bulbs
+int mycolors[] = {red,orange,yellow,green,blue,indigo,purple};
+int colorselect;
+int pos;              //positon of the encoder
+bool Mode1 = false;
+bool Mode2 = false;
+bool Mode3 = false;
 
 enum State {mode1, mode2, mode3};
 State _state = mode1;
@@ -30,7 +38,7 @@ Adafruit_NeoPixel pixels(12, 4, NEO_GRB + NEO_KHZ800);
 IPAddress ip(192,168,1,3);
 OneButton button1(upper, false);
 OneButton button2(lower, false);
-
+Encoder myEnc(B,A);
 void setup() {
   
 
@@ -40,7 +48,7 @@ void setup() {
   pixels.clear();
   Serial.print("LinkStatus: ");
   Serial.println(Ethernet.linkStatus());
-  Serial.println("Ready");
+  Serial.println("Connected");
   button1.attachClick(button1click);
   button2.attachClick(button2click);
   button1.attachDoubleClick(button1doubleclick);
@@ -48,6 +56,7 @@ void setup() {
   button1.attachLongPressStart(button1longpressstart);
   button2.attachLongPressStart(button2longpressstart);
   restrt();
+  Serial.println("Ready");
 
   
  
@@ -56,24 +65,31 @@ void setup() {
 void loop() {
   button1.tick();
   button2.tick();
+  poscheck();
+  checks();
   
 }
-
+ 
 void button1click() {
 
-  
+  color = colorselect;
+  Serial.print(color);
 }
 
 void button1doubleclick() {
   changebright = !changebright;
+  Serial.print("Brightness change: ");
+  Serial.println(Brightness);
+  pixels.clear();
+  pixels.show();
   
 }
 
 void button2click() { 
   lightpush();
-  Serial.println("lightsPushed");
+  Serial.println("lightsPushed: ");
   if (lightnumber  == 0) {
-    Serial.println("no lights on");
+    Serial.print("no lights on");
   }
 }
 
@@ -81,43 +97,43 @@ void button2doubleclick() { // changes the mode
 
   switch (_state) {
     case mode1:
-     _mode1();
+    
+     Serial.println("Mode1");
+     
+     Mode1 = !Mode1;
+     Mode2 = false;
+     Mode3 = false;
+     Serial.println(Mode1);
+     Serial.println(Mode3);
      _state = mode2;
     break;
     case mode2:
-     _mode2();
+    Mode2 = !Mode2;
+    Mode1 = false;
+    Mode3 = false;
+     pixels.clear();
+     pixels.show();
+     Serial.println("Mode2");
+     Serial.println(Mode2);
      _state = mode3;
     break;
     case mode3:
-     _mode3();
+    Mode1 = false;
+    Mode2 = false;
+    pixels.clear();
+    pixels.show();
+    Mode3 = !Mode3;
+     Serial.println(Mode3);
      _state = mode1;
-   
+     break;
   }
-  
 }
 
-void _mode1() {
- 
- 
-  Serial.println("mode1");
-  
-  
-}
-
-void _mode2() {
-  Serial.println("mode2");
-  
-}
-
-void _mode3() {
-  Serial.println("mode3");
-  
-}
 
 void button1longpressstart() {
   lightnumber = lightnumber +1;
-  Brightness = 255;
-  Serial.print ("lightnumber: ");
+  
+  Serial.print("lightnumber: ");
   Serial.println(lightnumber);
 }
 
@@ -127,17 +143,68 @@ void button2longpressstart() { // turns lights on/off state 0 = off
   Serial.println(lights);
 }
 
-void brightchange() {
-  if (changebright == true) {
-    pixels.clear();
-    
-    
+void brightchange() { //activated with button1 double click to change brightness
+  pixels.clear();
+  int i = 0;
+  for( i=0; i<12; i++){
+    pixels.setPixelColor(i, white);
   }
+  Brightness = map(pos, 0,96,0,255);
+  pixels.setBrightness(Brightness);
+  pixels.show();
+  }
+
+
+
+void poscheck() {          //checks the position of the encoder
+  pos = myEnc.read();
 }
 
 
+void checks() {
+  if (changebright == true) {
+    brightchange();
+  }
+
+    
+  
+  if (Mode1 == true) {
+    Mode2 = false;
+    Mode3 = false;
+    pixels.clear();
+    pixels.setBrightness(Brightness);
+    pixels.setPixelColor(2, blue);
+    pixels.show();
+  }
+
+  if (Mode2 == true) {
+    Mode1 = false;
+    Mode3 = false;
+     pixels.clear();
+    pixels.show();
+  }
 
 
+
+
+  
+  if (Mode3 == true) {
+    int i3;
+    int s = map(pos, 0,96,0,5);
+    for (i3=0; i3<12; i3++) {
+      pixels.setPixelColor(i3,mycolors[s]);
+    }
+    colorselect = HueRainbow[s];
+   
+    Mode2 = false;
+    Mode1 = false;
+    
+    
+    pixels.show(); 
+
+}
+}
+  
 
 
 
